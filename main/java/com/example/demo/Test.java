610feeebc57862c6bde025358c48d986162b4d1f
MiaomiaoShi1004 <123462116+MiaomiaoShi1004@users.noreply.github.com>
2023-03-16 11:11:30 +0900

    delete useless Test class

--- main/java/com/example/demo/Test.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-
-public class Test {
-    public static void main(String[] args) {
-        Ship ship = new Ship(150,100);
-//        pane.getChildren().add(ship.getCharacter());
-    }
-}
