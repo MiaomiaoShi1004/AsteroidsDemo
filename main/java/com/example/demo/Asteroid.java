f8cbbc47686813e3c4ec8573012a18a6675ff2b9
MiaomiaoShi1004 <123462116+MiaomiaoShi1004@users.noreply.github.com>
2023-04-20 21:41:50 +0100

    Different size of asteroid, different speed

--- main/java/com/example/demo/Asteroid.java
+++ main/java/com/example/demo/Asteroid.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import javafx.geometry.Point2D;
 import javafx.scene.shape.Polygon;
 
 import java.util.Random;
@@ -23,19 +24,42 @@ public class Asteroid extends Character{
         // set the random rotate
         Random rnd = new Random();
         super.getCharacter().setRotate(rnd.nextInt(360));
+        this.pentagonSize = pentagonSize;
+
         // set the random acceleration
         int accelerationAmount = 1 + rnd.nextInt(10);
         for (int i = 0; i < accelerationAmount; i++) {
             accelerate();
         }
-
         this.rotationalMovement = 0.5 - rnd.nextDouble();
-        this.pentagonSize = pentagonSize;
     }
+
+//    @Override
+//    public void move() {
+//        super.move();
+//        super.getCharacter().setRotate(super.getCharacter().getRotate() + rotationalMovement);
+//    }
 
     @Override
     public void move() {
+        // Calculate speed factor based on pentagonSize
+        double speedFactor = 0;
+        if (this.pentagonSize == 10) {
+            speedFactor = 1.8; // Fast speed
+        } else if (this.pentagonSize == 20) {
+            speedFactor = 1.5; // Medium speed
+        } else if (this.pentagonSize == 10) {
+            speedFactor = 1.0; // Slow speed (default)
+        }
+
+        // Apply speed factor to the movement
+        this.character.setTranslateX(this.character.getTranslateX() + this.movement.getX() * speedFactor);
+        this.character.setTranslateY(this.character.getTranslateY() + this.movement.getY() * speedFactor);
+
+        // Call the rest of the move() code from the Character class
         super.move();
+
+        // Update rotation as before
         super.getCharacter().setRotate(super.getCharacter().getRotate() + rotationalMovement);
     }
 }
